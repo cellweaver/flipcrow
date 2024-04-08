52093372ed0f70c44cd1ec57a51ffb883e8f29fd
electron river <4722975+greatergreenbio@users.noreply.github.com>
2024-04-08 08:57:22 -0400

    Add notebook code to embryonic library

--- src/flipcrow/scp1644_mothership.py
+++ src/flipcrow/scp1644_mothership.py
@@ -28,6 +28,63 @@ from scanpy import _version
 
 print(sc._version.version)
 
+moffitt_pdac = {
+    'BASAL': [
+        'VGLL1', 
+        'UCA1', 
+        'S100A2', 
+        'LY6D', 
+        'SPRR3',
+        'SPRR1B',
+        'LEMD1',
+        'KRT15',
+        'CTSV', # 'CTSL2',
+        'DHRS9',
+        'AREG',
+        'CST6',
+        'SERPINB3',
+        # 'KRT6C', # not present in var_names
+        'KRT6A',
+        'FAM83A',
+        'SCEL',
+        'FGFBP1',
+        'KRT7',
+        'KRT17',
+        'GPR87',
+        'TNS4',
+        'SLC2A1',
+        'ANXA8L2',
+    ],
+    'CLASSICAL': [
+        'BTNL8',
+        'FAM3D',
+        'PRR15L', # 'ATAD4',
+        'AGR3',
+        'CTSE',
+        # 'TMEM238L', # 'LOC400573', not present in genes
+        'LYZ',
+        'TFF2',
+        'TFF1',
+        'ANXA10',
+        'LGALS4',
+        'PLA2G10',
+        'CEACAM6',
+        'VSIG2',
+        'TSPAN8',
+        'ST6GALNAC1',
+        'AGR2',
+        'TFF3',
+        'CYP3A7',
+        'MYO1A',
+        'CLRN3',
+        'KRT20',
+        'CDH17',
+        'SPINK4',
+        'REG4',
+    ],
+}
+
+
 
 def load_scp1644_data(check_metadata: bool = True) -> ad.AnnData:
     scp1644_small_csv = flipcrow.paths.DATA_PATH / 'SCP1644/expression/Biopsy473_RawDGE_1370cells.csv'
@@ -238,6 +295,174 @@ def norm_log_xform_scp1644_data(input: ad.AnnData) -> None:
     return sc.pp.log1p(tmp, copy=True)
     
 
+def kruskal_wallis_tests(input: ad.AnnData): # -> Tuple[ad.AnnData, Mapping[str, KruskalType,....
+    samples = [input[input.obs.leiden == cluster_id, input.var.highly_variable] for cluster_id in input.obs.leiden.unique()]
+    others = [input[input.obs.leiden != cluster_id, input.var.highly_variable] for cluster_id in input.obs.leiden.unique()]
+
+    print("Performing KW test")
+    kw_map = {}
+    for idx, gene in enumerate(input.var_names[input.var.highly_variable]):
+        
+        kw_map[gene] = scipy.stats.kruskal(*[x[:, gene].X.squeeze() for x in samples])
+        if idx % 100 == 0:
+            print(gene, kw_map[gene])
+
+    print(len(kw_map))
+    kw_srs = pd.Series(kw_map)
+    input.var['kw_statistic'] = np.zeros(len(input.var_names)) 
+    input.var['kw_pval'] = np.ones(len(input.var_names)) 
+
+    input.var['kw_statistic'].loc[kw_map.keys()] = [x.statistic for x in kw_map.values()]
+    input.var['kw_pval'].loc[kw_map.keys()] = [x.pvalue for x in kw_map.values()]            
+
+    # performs pairwise significance tests
+    print("Performing pairwise significance tests")
+    
+    buf_others = []
+    buf_zeros = []
+
+    start_gene_time = time.time()
+    for idx in range(len(samples)):
+        # print(idx)
+        start_mean_time = time.time()
+        buf_others.append(scipy.stats.mannwhitneyu(samples[idx].X, others[idx].X))
+        buf_zeros.append(scipy.stats.mannwhitneyu(samples[idx].X, np.zeros(samples[idx].X.shape), alternative='greater'))
+        end_zeros_time = time.time()
+        print(idx, (end_zeros_time - start_mean_time))    
+    end_gene_time = time.time()
+    print('done', (end_gene_time - start_gene_time))
+    
+    mwm_others_df = pd.DataFrame([x.pvalue for x in buf_others], index=input.obs.leiden.unique(), columns=input.var['highly_variable'][input.var['highly_variable']].index)
+    mwm_zeros_df = pd.DataFrame([x.pvalue for x in buf_zeros], index=input.obs.leiden.unique(), columns=input.var['highly_variable'][input.var['highly_variable']].index)
+
+    mwm_others_pval_cutoff = 1.0e-9
+    mwm_zeros_pval_cutoff = 1.0e-5
+
+    mwm_mean_pass = mwm_others_df < mwm_others_pval_cutoff
+    mwm_zeros_pass = mwm_zeros_df < mwm_zeros_pval_cutoff
+
+    print(mwm_mean_pass & mwm_zeros_pass)
+
+    return input.copy(), kw_map, mwm_others_df, mwm_zeros_df
+
+
+
+    def plot_cluster_gene_distr_1d(samples, others, cluster_key, gene_key, plot=True):
+
+        coi = int(cluster_key)
+        samples_hist = np.histogram(samples[coi][:, gene_key].X.squeeze(), bins=20)
+        others_hist = np.histogram(others[coi][:, gene_key].X.squeeze(), bins=20)
+        # print(samples_hist)
+        if plot:
+            plt.plot(others_hist[1][:-1], others_hist[0]/others_hist[0].sum(), color='r')
+            plt.plot(samples_hist[1][:-1], samples_hist[0]/samples_hist[0].sum(), color='b')
+            plt.axis([0, 3.5, 0, 0.2])
+        
+        this_cluster = sc.get.rank_genes_groups_df(input, str(cluster_key))
+        join_cluster = this_cluster.join(input.var.highly_variable, on='names')
+        print(f"{gene_key} in {cluster_key}: {gene_key in join_cluster.names.values}")
+        
+        if gene_key in chomp.names.values:
+            print(chomp.loc[chomp.names == gene_key, :])
+
+        return this_cluster
+
+# TODO:  plot co-expression by chromosomal location
+
+
+def check_rank_genes_groups_df(input: ad.AnnData, coi: Union[int, str], focus_prefix=None) -> None: 
+    coi = '34'
+    rgg_df = sc.get.rank_genes_groups_df(input, coi)
+    print(sc.get.rank_genes_groups_df(input, coi).iloc[:30, :])
+    if focus_prefix:
+        with pd.option_context('display.min_rows', 50):
+            print(rgg_df.loc[[x.startswith(focus_prefix) for x in rgg_df.names.values], :])
+
+    print('\n'.join(sc.get.rank_genes_groups_df(input, coi).names[:100]))
+
+
+def basal_classical_analysis(input: ad.AnnData) -> None:
+    sc.tl.score_genes(input, moffitt_pdac['CLASSICAL'], score_name='classical_score', ctrl_size=100)
+    sc.tl.score_genes(input, moffitt_pdac['BASAL'], score_name='basal_score', ctrl_size=100)
+    input.obs.loc[:, 'diff_score'] = input.obs.classical_score - input.obs.basal_score
+    plt.plot(input.obs.classical_score, input.obs.basal_score, 'o')
+
+
+def compare_classical_basal_genes(input: ad.AnnData, classical_gene: str, basal_gene: str) -> None:
+
+    plt.plot(input.obs.classical_score, input.to_df().loc[:, classical_gene].values, '.')
+    plt.plot(input.obs.basal_score, input.to_df().loc[:, classical_gene].values, '.')
+
+
+    plt.figure(1)
+    print(f'{classical_gene} (Classical)')
+    print('Pearson Classical', scipy.stats.pearsonr(input.obs.classical_score, input.to_df().loc[:, classical_gene].values))
+    print('Pearson Basal', scipy.stats.pearsonr(input.obs.basal_score, input.to_df().loc[:, classical_gene].values))
+
+    print('Spearman Classical', scipy.stats.spearmanr(input.obs.classical_score, input.to_df().loc[:, classical_gene].values))
+    print('Spearman Basal', scipy.stats.spearmanr(input.obs.basal_score, input.to_df().loc[:, classical_gene].values))
+
+    print()
+    plt.figure(2)
+    plt.plot(input.obs.classical_score, input.to_df().loc[:, basal_gene].values, '.')
+    plt.plot(input.obs.basal_score, input.to_df().loc[:, basal_gene].values, '.')
+
+    print(f'{basal_gene} (Basal)')
+    print('Pearson Classical', scipy.stats.pearsonr(input.obs.classical_score, input.to_df().loc[:, basal_gene].values))
+    print('Pearson Basal', scipy.stats.pearsonr(input.obs.basal_score, input.to_df().loc[:, basal_gene].values))
+
+    print('Spearman Classical', scipy.stats.spearmanr(input.obs.classical_score, input.to_df().loc[:, basal_gene].values))
+    print('Spearman Basal', scipy.stats.spearmanr(input.obs.basal_score, input.to_df().loc[:, basal_gene].values))
+
+
+def tumor_cleanup(input: ad.AnnData) -> None:
+    sc.pl.pca(input, color=['leiden', 'donor_ID'], size=50)
+
+    clean_input = input[[x not in ['PANFR0580', 'PANFR0588', 'PANFR0543'] for x in input.obs.donor_ID], :]
+    sc.pp.highly_variable_genes(clean_input)
+    sc.pp.pca(clean_input)
+    sc.pl.pca(clean_input, color=['leiden', 'donor_ID'], size=50)
+    sc.pp.neighbors(clean_input, n_neighbors=35)
+    sc.tl.tsne(clean_input)
+    sc.tl.leiden(clean_input, resolution=3.)
+    sc.pl.tsne(clean_input, color=['leiden', 'donor_ID'], size=100, legend_loc="on data")
+
+    print(len(clean_input.obs.donor_ID.unique()))
+    print(Counter(clean_input.obs.donor_ID))
+    with pd.option_context('display.min_rows', 50):
+        print(pd.DataFrame(clean_input.varm['PCs'][:, 0:3], index=clean_input.var_names).sort_values(0))
+    sc.pl.tsne(clean_input, color='pct_counts_mt')
+
+    mttrim_input = clean_input[clean_input.obs.pct_counts_mt <= 20, :]
+    sc.pp.highly_variable_genes(mttrim_input)
+    sc.pp.pca(mttrim_input)
+    sc.pp.neighbors(mttrim_input)
+    sc.tl.tsne(mttrim_input)
+    sc.tl.leiden(mttrim_input, resolution=2.)
+    sc.pl.pca(mttrim_input, color=['leiden', 'donor_ID'], size=100, legend_loc='on data')
+    sc.pl.tsne(mttrim_input, color=['leiden', 'donor_ID'], size=100, legend_loc='on data')
+
+    print(Counter(mttrim_input.obs.donor_ID))
+    sc.tl.umap(mttrim_input)
+
+    # PC0 is EMT :) :)
+    # PC1 is Classical :)
+    # PC2 is Basal :)
+
+    with pd.option_context('display.min_rows', 50):
+        print(pd.DataFrame(mttrim_input.varm['PCs'][:, 0:3], index=mttrim_input.var_names).sort_values(2))    
+
+    for x in range(3):
+        print('\n'.join(pd.DataFrame(mttrim_input.varm['PCs'], index=mttrim_input.var_names).sort_values(x, ascending=False).index[:50]))
+
+    sc.tl.score_genes(mttrim_input, moffitt_pdac['CLASSICAL'], score_name='classical_score', ctrl_size=100)
+    sc.tl.score_genes(mttrim_input, moffitt_pdac['BASAL'], score_name='basal_score', ctrl_size=100)
+    mttrim_input.obs.loc[:, 'diff_score'] = mttrim_input.obs.classical_score - mttrim_input.obs.basal_score
+
+    sc.pl.heatmap(mttrim_input, moffitt_pdac, 'diff_score', swap_axes=True, vmin=-1.5, vmax=3)
+
+    return 
+
 
 if __name__ == "__main__":
     scp1644_raw = load_scp1644_data(check_metadata=True)
@@ -247,3 +472,10 @@ if __name__ == "__main__":
     print(scp1644_raw)
     print(scp1644_trim)
     print(scp1644_log)
+
+    cluster_key = '10'
+    gene_key = 'A1CF'
+
+    test = plot_cluster_gene_distr_1d(cluster_key, gene_key)
+    print()
+    print(test)    
